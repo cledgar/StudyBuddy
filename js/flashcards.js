d34c29fbf897091fe66e90580b0e4cc55f96e4b0
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:41:27 -0600

    Add pseudocode mockup to flashcards.js

--- js/flashcards.js
+++ js/flashcards.js
@@ -1,1 +1,30 @@
 // flashcard logic
+
+class Flashcards {
+  constructor() {
+    this.cards = [];  // { question: "...", answer: "..." }
+    this.currentCard = null;
+  }
+
+  loadCards(array) {
+    this.cards = array;
+  }
+
+  randomCard() {
+    this.currentCard = chooseRandom(this.cards);
+    return this.currentCard.question;
+  }
+
+  checkAnswer(userAnswer) {
+    if (userAnswer.trim().toLowerCase() === 
+        this.currentCard.answer.toLowerCase()) {
+      // might wannt to remove this and/or add an option for users to simply check their 
+      // own answers in case they have a topic that's hard to type exact answers in for
+
+      player.coins += 5;   // reward
+      saveGameData();
+      return true;
+    }
+    return false;
+  }
+}
