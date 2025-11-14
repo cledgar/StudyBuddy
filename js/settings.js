1dcaea4c0237e64463e137d1af954356a4909151
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:41:53 -0600

    Add pseudocode to settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,22 @@
 // logic for the settings page
+
+onSettingsLoad() {
+  loadGameData();
+  populateFlashcardEditor();
+  populatePetOptions();
+}
+
+addFlashcard(question, answer) {
+  gameData.flashcards.push({ question, answer });
+  saveGameData();
+}
+
+deleteFlashcard(index) {
+  gameData.flashcards.splice(index, 1);
+  saveGameData();
+}
+
+changePetType(newType) {
+  gameData.pet.type = newType;
+  saveGameData();
+}
