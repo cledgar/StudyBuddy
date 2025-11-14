a948447876e0e721e04ef82ec594c7e2ed62dac8
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:35:41 -0600

    Add code base to pet.js

--- js/pet.js
+++ js/pet.js
@@ -1,1 +1,44 @@
 // pet logic, such as needs and anything else we want to add
+
+//also feel free to get rid of anything if you dont think we'll need it, or add whatever! + feel free to stub out anything we dont have the resources for yet
+
+class Pet {
+  constructor() {
+    this.name = "Your Pet";
+    this.type = "cat";
+
+    // ranges 0–100
+    this.hunger = 100;
+    this.thirst = 100;
+    this.cleanliness = 100;
+  }
+
+  updateNeeds() {
+    this.hunger -= random(1, 5);
+    this.thirst -= random(1, 5);
+    this.cleanliness -= random(0, 2);
+
+    clampValuesToZero();
+  }
+
+  feed() {
+    if (inventory.has("food")) {
+      inventory.remove("food");
+      this.hunger += 25;
+    }
+  }
+
+  giveWater() {
+    if (inventory.has("water")) {
+      inventory.remove("water");
+      this.thirst += 25;
+    }
+  }
+
+  brush() {
+    if (inventory.has("brush")) {
+      inventory.remove("brush");
+      this.cleanliness += 30;
+    }
+  }
+}
