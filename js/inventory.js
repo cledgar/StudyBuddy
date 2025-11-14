e42bdef01bcbfef834a76b6b082d16271cd4ff4e
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:38:00 -0600

    Add code base to inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -1,1 +1,30 @@
 // inventory logic
+
+//same as before, remove whatever you want! :)
+
+class Inventory {
+  constructor() {
+    this.items = {}; // ex: { food: 3, water: 1 }
+  }
+
+  add(itemName, amount=1) {
+    if (!this.items[itemName]) this.items[itemName] = 0;
+    this.items[itemName] += amount;
+    saveGameData();
+  }
+
+  remove(itemName, amount=1) {
+    if (this.items[itemName] >= amount) {
+      this.items[itemName] -= amount;
+      saveGameData();
+    }
+  }
+
+  has(itemName) {
+    return this.items[itemName] > 0;
+  }
+
+  renderInventoryUI() {
+    // loop through items and draw them in a modal
+  }
+}
