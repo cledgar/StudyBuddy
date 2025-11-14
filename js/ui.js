42d8fd68d774e59b03cf27c898abfdd04a2adbb3
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:42:35 -0600

    Add pseudocode to ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,20 @@
 // ui helper functions
+
+updatePetUI() {
+  setText("#hunger", "Hunger: " + pet.hunger + "%");
+  setText("#thirst", "Thirst: " + pet.thirst + "%");
+  setText("#clean", "Cleanliness: " + pet.cleanliness + "%");
+}
+
+renderCoinDisplay() {
+  setText("#coin-display", "Coins: " + player.coins);
+}
+
+showModal(htmlContent) {
+  modalContainer.innerHTML = htmlContent;
+  modalContainer.classList.remove("hidden");
+}
+
+hideModal() {
+  modalContainer.classList.add("hidden");
+}
