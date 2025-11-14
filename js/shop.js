a8da978ac7114724c9f788a66f502df6141df76f
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:38:39 -0600

    Add code base to shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,1 +1,23 @@
 // shop logic
+
+class Shop {
+  constructor(itemListJSON) {
+    this.items = itemListJSON; 
+    // Example item = { id:"food", price:10 }
+  }
+
+  buy(itemId) {
+    let item = this.items[itemId];
+
+    if (player.coins >= item.price) {
+      player.coins -= item.price;
+      inventory.add(itemId);
+      saveGameData();
+      renderCoinDisplay();
+    }
+  }
+
+  renderShopUI() {
+    // show each shop item with a buy button
+  }
+}
