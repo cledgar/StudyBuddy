86759d757b4dcd687ec15805e43d127cd427b3b3
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-14 12:33:48 -0600

    Add code base to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-// main game loop & event listeners
+// main game loop & event listener pseudocode mockup
+
+onPageLoad() {
+  loadGameData();
+  renderUI();
+  startGameLoop();
+}
+
+startGameLoop() {
+  setInterval(() => {
+    pet.updateNeeds();
+    updatePetUI();
+  }, 30000);   // every 30 seconds
+}
