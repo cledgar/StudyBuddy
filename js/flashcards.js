3dbe7b926727f25ce90a68e0dac577b5e1d272da
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-16 01:35:16 -0600

    Add instructions to flashcard.js

--- js/flashcards.js
+++ js/flashcards.js
@@ -1,5 +1,8 @@
 // flashcard logic
 
+// need flashcard functionality-- ability to flip from question to answer, and a way to score points by being correct. 
+// this can be done with typing in a correct answer or self-confirmation. up to you all doing backend to decide this
+
 class Flashcards {
   constructor() {
     this.cards = [];  // { question: "...", answer: "..." }
