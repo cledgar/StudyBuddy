500f8731b53a02043fd131cd8cae03680e6f1e0c
cedgar <114725339+cledgar@users.noreply.github.com>
2025-11-16 01:42:52 -0600

    add instructions to storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,8 @@
 // optional, we can delete this if we want, but this would allow players to save data in the local storage. focus on this only if we have time
 
+// if you end up working on this, like i said, this should be the save and load features for the players, including pet type, name, inventory
+// money, flashcards, etc
+
 saveGameData() {
   let data = {
     pet: pet,
